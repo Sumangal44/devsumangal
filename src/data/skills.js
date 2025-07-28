f7f079c05347c038b1600c01b2da1310c5cea0b1
Avhishikta maiti <avhishiktamaiti@gmail.com>
2025-07-28 22:44:40 +0530

    Update skills.js

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,8 +1,7 @@
 export const skills = [
   { name: "HTML", icon: "https://cdn.simpleicons.org/html5/E34F26" },
   { name: "CSS", icon: "https://cdn.simpleicons.org/css3/1572B6" },
-  { name: "JavaScript", icon: "https://cdn.simpleicons.org/javascript/F7DF1E" },
-      name: "Tailwind CSS",
+        name: "Tailwind CSS",
     icon: "https://cdn.simpleicons.org/tailwindcss/06B6D4"
   },
   
