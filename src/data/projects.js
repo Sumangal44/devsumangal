bb88c6ac5509899f87b15e9f044e9d316bdb8dda
sumangal44 <sumangalkaran44@gmail.com>
2026-05-02 19:29:18 +0530

    feat: add comprehensive UI component library and implement core project pages

---
+++ src/data/projects.js
@@ -0,0 +1,30 @@
+
+export const projects = [
+  {
+    title: "Charmmono-Theme",
+    description:
+      "🚀👨‍🏭❤️Charmmono-Theme is a sleek VS Code theme inspired by monospace aesthetics and Charmed Icons, offering a minimal, elegant, and highly readable coding experience with dark & light modes. 🚀",
+    image:
+      "https://images.vscodethemes.com/sumangalkaran.charmmono-theme/charmmono-theme-dark-js-preview-BIg6.svg",
+    link: "https://marketplace.visualstudio.com/items?itemName=sumangalkaran.charmmono-theme"
+  },
+  {
+    title: "skelecli.dev",
+    description:
+      "🚀❤️✅A simple CLI to generate a blueprint for a new project✅👨‍🏭🚀",
+    image: "https://res.cloudinary.com/dmeroa8nx/image/upload/v1747563040/Screenshot_2025-05-18_153617_mv5fla.png",
+    link: "https://skelecli-dev.vercel.app/"
+  },
+  {
+    title: "javaprojects",
+    description: "🚀 Java Projects – A collection of beginner to advanced Java programs 🎯💻",
+    image: "https://res.cloudinary.com/dmeroa8nx/image/upload/v1747564163/javaprojects-zeta.vercel.app_Laptop_2025-05-18_10.28.41_fufdrc.png",
+    link: "https://javaprojects-zeta.vercel.app"
+  },
+  {
+    title:"Devsumangal",
+    description: "🚀👨‍💻❤️Devsumangal is a personal portfolio website showcasing my projects, skills, and experiences in web development. It features a clean design, responsive layout, and easy navigation to highlight my work effectively. 🚀",
+    image:"https://res.cloudinary.com/dmeroa8nx/image/upload/v1748770202/devsumangal.netlify.app_Laptop_2025-06-01_09.28.42_dpzbbf.png",
+    link: "https://devsumangal.netlify.app/",
+  }
+];
