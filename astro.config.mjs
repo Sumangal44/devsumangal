c6352093f21f7c8267dfe54590359b13bbc46ca4
sumangal44 <sumangalkaran44@gmail.com>
2026-05-02 18:25:38 +0530

    feat: add React, Partytown, Markdoc, and Sitemap integrations to Astro project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import partytown from '@astrojs/partytown';
+
+import markdoc from '@astrojs/markdoc';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), partytown(), markdoc(), sitemap()]
+});
